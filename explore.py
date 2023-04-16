a644ef584e0c0857ec48b09665fc82e682dc7359
Fermin Garcia <fermin.f.garcia1@gmail.com>
2023-04-16 17:13:37 -0500

    added more functions to help with the explore phase

--- explore.py
+++ explore.py
@@ -1,12 +1,12 @@
-# import data acquisition functions
+# import data acquisition functions, visualization tools, and 
 import wrangle as w
-# import data tools
 import numpy as np
 import pandas as pd
-# import visualization tools
 from scipy import stats
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.cluster import KMeans
+import pandas as pd
 
 def check_hypothesis(data, x, y, α=0.05, test=stats.pearsonr):
     '''
@@ -50,3 +50,78 @@ def get_plot_alcohol_by_quantity(train):
     plt.ylabel('Quality Score of the Wine', size=14)
     # show the plot
     plt.show()
+	
+	
+def find_k(X_train, cluster_vars, k_range):
+    sse = []
+    for k in k_range:
+        kmeans = KMeans(n_clusters=k)
+
+        # X[0] is our X_train dataframe..the first dataframe in the list of dataframes stored in X. 
+        kmeans.fit(X_train)
+#i think this part might need to be X_train[cluster_vars],
+# otherwise it is looking at clustering all of the columns together
+
+        
+        
+        # inertia: Sum of squared distances of samples to their closest cluster center.
+        sse.append(kmeans.inertia_) 
+
+    # compute the difference from one k to the next
+    delta = [round(sse[i] - sse[i+1],0) for i in range(len(sse)-1)]
+
+    # compute the percent difference from one k to the next
+    pct_delta = [round(((sse[i] - sse[i+1])/sse[i])*100, 1) for i in range(len(sse)-1)]
+
+    # create a dataframe with all of our metrics to compare them across values of k: SSE, delta, pct_delta
+    k_comparisons_df = pd.DataFrame(dict(k=k_range[0:-1], 
+                             sse=sse[0:-1], 
+                             delta=delta, 
+                             pct_delta=pct_delta))
+
+    # plot k with inertia
+    plt.plot(k_comparisons_df.k, k_comparisons_df.sse, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('SSE')
+    plt.title('The Elbow Method to find the optimal k\nFor which k values do we see large decreases in SSE?')
+    plt.show()
+
+    # plot k with pct_delta
+    plt.plot(k_comparisons_df.k, k_comparisons_df.pct_delta, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Percent Change')
+    plt.title('For which k values are we seeing increased changes (%) in SSE?')
+    plt.show()
+
+    # plot k with delta
+    plt.plot(k_comparisons_df.k, k_comparisons_df.delta, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Absolute Change in SSE')
+    plt.title('For which k values are we seeing increased changes (absolute) in SSE?')
+    plt.show()
+
+    return k_comparisons_df
+	
+def cluster_columns(scaled_df, n_clusters):
+    # Initialize an empty dictionary to store the cluster labels for each column
+    clusters_dict = {}
+
+    # Loop over each column in the scaled DataFrame
+    for col in scaled_df.columns:
+        # Initialize a k-means model with n_clusters
+        kmeans = KMeans(n_clusters=n_clusters)
+
+        # Fit the model to the column data
+        kmeans.fit(scaled_df[col].values.reshape(-1, 1))
+
+        # Get the cluster labels for each data point in the column
+        col_clusters = kmeans.labels_
+
+        # Add the cluster labels to the clusters_dict with the column name as the key
+        clusters_dict[col] = col_clusters
+
+    # Create a new DataFrame from the clusters_dict
+    clusters_df = pd.DataFrame(clusters_dict)
+
+    # Return the clusters_df
+    return clusters_df
